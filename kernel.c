5a62e5098fbcd6e8dbc095d33a2ed3511c7069cf
coursework <coursework@coursework.csc.tntech.edu>
2024-09-22 01:40:28 -0500

    got check box function to work

--- kernel.c
+++ kernel.c
@@ -1,12 +1,26 @@
 void putc_to(unsigned int row, unsigned int col, const char c);
+void clear_src(unsigned int startRow, unsigned int startColumn, unsigned int endRow, unsigned int endColumn);
+void box(unsigned int startRow, unsigned int startColumn, unsigned int endRow, unsigned int endColumn);
+int term_txtheight();
+int term_txtwidth();
+
 
 int main() {
+	unsigned int startRow = 0;
+	unsigned int startColumn = 0;
+	unsigned int endRow = 5;
+	unsigned int endColumn = 5;
+	clear_src(startRow, startColumn, endRow, endColumn);
 
-	const char c = 'x';
-	
-	for(int x = 0; x < 500; ++x) {
-		for(int y = 0; y < 500; ++y) {
-			putc_to(x, y, c);	
+	box(startRow, startColumn, endRow, endColumn);
+}
+
+void clear_src(unsigned int startRow, unsigned int startColumn, unsigned int endRow, unsigned int endColumn) {
+
+	const char c = ' ';
+	for(unsigned int x = startRow; x < endRow; ++x) {
+		for(unsigned int y = startColumn; y < endColumn; ++y) {
+			putc_to(x, y, c);
 		}
 	}
 }
