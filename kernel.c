a7833623508ee9853b9120d789930e1af9a59543
coursework <coursework@coursework.csc.tntech.edu>
2024-09-22 21:43:26 -0500

    finished? need to check everything there was an error after I made comments?

--- kernel.c
+++ kernel.c
@@ -1,20 +1,36 @@
-void putc_to(unsigned int row, unsigned int col, const char c);
-void clear_src(unsigned int startRow, unsigned int startColumn, unsigned int endRow, unsigned int endColumn);
-void box(unsigned int startRow, unsigned int startColumn, unsigned int endRow, unsigned int endColumn);
-int term_txtheight();
-int term_txtwidth();
+/*
+	Author: Jesus Villanueva-Segovia
+	Name: kernel.c
+	Purpose: This will serve as the driver for our operating system? It just draws a box and hello string right now
+*/
 
 
+//--Prototypes
+void putc_to(unsigned int row, unsigned int col, const char c); //Prototype to print a character, defined in libos.a
+void print_to(unsigned int row, unsigned int col, const char[]); //Prototype to print some text, defined in libos.a
+void clear_src(unsigned int startRow, unsigned int startColumn, unsigned int endRow, unsigned int endColumn); //Prototype to clear at a ordered pair, defined in libos.a
+void box(unsigned int startRow, unsigned int startColumn, unsigned int endRow, unsigned int endColumn); //Prototype to draw a box, defined in box.S
+int term_txtheight(); //Prototype to find the maximum height of the terminal, defined in libos.a
+int term_txtwidth(); //Prototype to find the maximum width of the terminal, defined in libos.a
+
+
+//--Main 
 int main() {
-	unsigned int startRow = 0;
-	unsigned int startColumn = 0;
-	unsigned int endRow = 5;
-	unsigned int endColumn = 5;
-	clear_src(startRow, startColumn, endRow, endColumn);
+	//defining our boundaries
+	unsigned int startRow = 21;
+	unsigned int startColumn = 49;
+	unsigned int endRow = 27;
+	unsigned int endColumn = 79;
+	clear_src(startRow, startColumn, endRow, endColumn); // clearing the section that we draw in
 
-	box(startRow, startColumn, endRow, endColumn);
+	box(startRow, startColumn, endRow, endColumn); // drawing the box
+	
+	print_to(24, 59, "Hello World!"); //printing hello world
 }
 
+// Name: clear_src
+// Arguments: unsigned int srow, unsigned int scol, unsigned int erow, unsigned int ecol
+// Purpose: This will clear a defined section of the terminal
 void clear_src(unsigned int startRow, unsigned int startColumn, unsigned int endRow, unsigned int endColumn) {
 
 	const char c = ' ';
